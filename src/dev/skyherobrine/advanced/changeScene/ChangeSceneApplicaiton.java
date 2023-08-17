f7ec50ba9dfd0a156563e4b628c6104c0754f998
BuildTools <unconfigured@null.spigotmc.org>
2023-08-17 18:05:44 +0700

    tuỳ chỉnh controller MediaView và thêm đoạn code mẫu Advanced Switch-Scene

--- src/dev/skyherobrine/advanced/changeScene/ChangeSceneApplicaiton.java
+++ src/dev/skyherobrine/advanced/changeScene/ChangeSceneApplicaiton.java
@@ -1,13 +1,37 @@
 package dev.skyherobrine.advanced.changeScene;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ChangeSceneApplicaiton extends Application {
 
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToScene1(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("scene_1.fxml"));
+		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void switchToScene2(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("scene_2.fxml"));
+		stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
 	@Override
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
